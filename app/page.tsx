c0668cd49fcd35bbad4949cdbdfe0ab7670d2615
Guy Goldman <guy.goldman1@mail.huji.ac.il>
2024-02-03 17:47:41 +0200

    Your commit message here

--- app/page.tsx
+++ app/page.tsx
@@ -1,54 +1,43 @@
-import WebsiteScreen from '@/lib/components/website-screen';
-import { COURSE_WELCOME_TEXT } from '@/lib/config';
-import Image from 'next/image';
-import Link from 'next/link';
+  import WebsiteScreen from '@/lib/components/website-screen';
+  import { COURSE_WELCOME_TEXT } from '@/lib/config';
+  import Image from 'next/image';
+  import Link from 'next/link';
+  import React from 'react';
+  import Box from '@mui/material/Box';
+  import Grid from '@mui/material/Grid';
+  import Bar from './components/Bar';
+  import Calendar from './components/calendar';
 
-export default function Home() {
-  return (
-    <WebsiteScreen>
-      <div className="grid">
-        <div className="center padding-2 text-xl">
-          <span>
-            {COURSE_WELCOME_TEXT}
-          </span>
-        </div>
-        <div className="center padding-2">
-          <Image
-            src="/huji.png"
-            alt="HUJI Logo"
-            width={320}
-            height={100}
-            priority
-          />
-        </div>
-        <div className="center padding-2">
-          <Image
-            src="/bezalel.png"
-            alt="Bezalel Logo"
-            className="item"
-            width={320}
-            height={100}
-            priority
-          />
-        </div>
-        <div>
-          <h1 className="center text-xxxl">
-            Lorem Ipsum
-          </h1>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec faucibus sit amet nunc nec vehicula. Fusce ornare nec nulla non imperdiet. Fusce vel sodales justo. Sed efficitur arcu lorem, at iaculis odio ultricies et. Duis rutrum urna nec elit bibendum, sed hendrerit nulla posuere. Vestibulum vestibulum, ante non tincidunt posuere, dui arcu lacinia nisl, nec rhoncus massa arcu ac ipsum. Nam congue interdum tortor, eu dignissim massa scelerisque vitae. Sed ultricies bibendum congue. Praesent non magna id ligula maximus luctus. Donec vitae nibh quis neque luctus sagittis et eget nunc. Aliquam id ullamcorper lacus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Curabitur tempor quis dolor a lacinia.
-          </p>
-          <ul>
-            <li>Aliquam maximus tellus sed lacus venenatis, ac cursus eros mollis.</li>
-            <li>In id ante sed sem pharetra molestie et vitae arcu.</li>
-            <li>Cras pharetra turpis at pretium elementum.</li>
-            <li>Donec ultrices felis vel lectus auctor iaculis.</li>
-          </ul>
-          <p>
-            See our <Link target="_blank" href="https://github.com/digital-product-jam-2024/course/tree/main/reference">Reference Material</Link> on <Link target="_blank" href="https://github.com/digital-product-jam-2024/course/blob/main/reference/html.md">HTML</Link>, <Link target="_blank" href="https://github.com/digital-product-jam-2024/course/blob/main/reference/css.md">CSS</Link>, and <Link target="_blank" href="https://github.com/digital-product-jam-2024/course/blob/main/reference/javascript.md">JavaScript</Link>.
-          </p>
-        </div>
+  export default function DenseAppBar() {
+    return (
+      <div>
+        <Bar></Bar>
+      <div >
+      <Box sx={{ display: 'flex', gap: 5 }}>
+          <Grid container spacing={5} sx={{ height: '400px' }}>
+            <Grid item xs={3} md={3}>
+              <h1 id="calendar" style={{ height: '100%' }}>
+                Calendar
+                <Calendar></Calendar>
+              </h1>
+            </Grid>
+            <Grid item xs={9} md={9}>
+              <h1 id="mainpage" style={{ height: '100%' , position: 'relative' }}>
+                The main part of the page - questions and explanations
+                <p id= "next" style={{ position: 'absolute', bottom: 0, left: 25 }}>
+                Next
+              </p>
+              </h1>
+            </Grid>
+          </Grid>
+        </Box>
       </div>
-    </WebsiteScreen>
-  )
-}
+      <div>
+        <h1 id="timeline">
+          Timeline         
+          
+        </h1>
+      </div>
+    </div>
+    );
+  }
