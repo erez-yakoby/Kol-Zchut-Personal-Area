54ee41ad74f9a6bf7f5a366c57c701a4487fc324
Guy Goldman <guy.goldman1@mail.huji.ac.il>
2024-02-05 16:47:16 +0200

    Your commit message here

--- app/page.tsx
+++ app/page.tsx
@@ -4,39 +4,25 @@
   import Link from 'next/link';
   import React from 'react';
   import Box from '@mui/material/Box';
-  import Grid from '@mui/material/Grid';
-  import Bar from './components/Bar';
+  import Bar from './components/Bar/Bar';
   import Calendar from './components/calendar';
+  import Paper from '@mui/material/Paper';
+  import ProgressBar from './components/progressbar';
+  import Main from './components/Main/Main';
 
   export default function DenseAppBar() {
     return (
       <div>
         <Bar></Bar>
-      <div >
-      <Box sx={{ display: 'flex', gap: 5 }}>
-          <Grid container spacing={5} sx={{ height: '400px' }}>
-            <Grid item xs={3} md={3}>
-              <h1 id="calendar" style={{ height: '100%' }}>
-                Calendar
-                <Calendar></Calendar>
-              </h1>
-            </Grid>
-            <Grid item xs={9} md={9}>
-              <h1 id="mainpage" style={{ height: '100%' , position: 'relative' }}>
-                The main part of the page - questions and explanations
-                <p id= "next" style={{ position: 'absolute', bottom: 0, left: 25 }}>
-                Next
-              </p>
-              </h1>
-            </Grid>
-          </Grid>
-        </Box>
+      <div>
+        <Main></Main>
       </div>
       <div>
+        <Box>
         <h1 id="timeline">
-          Timeline         
-          
+          Timeline 
         </h1>
+        </Box>
       </div>
     </div>
     );
