5176c15ca79d9e93d083be9b3055d5fd6cff4259
Guy Goldman <guy.goldman1@mail.huji.ac.il>
2024-02-21 13:14:55 +0200

    fonts and buttons

--- app/components/Tasks/Question.jsx
+++ app/components/Tasks/Question.jsx
@@ -1,20 +1,35 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "@mui/material";
 
 const Question = ({ question }) => {
+  const [active, setActive] = useState(null);
+
+  const handleButtonClick = (index) => {
+    setActive(index === active ? null : index);
+  };
+
   const answers = question.possibleAnswers;
   console.log(answers);
   return (
-    <div className="flexRow gap-30 rtl">
+    <div className="flexRow gap-60 rtl ">
       {answers.map((answer, index) => {
         return (
-          <button key={index} className="questionBtn">
-            {answer}
+          <button
+          key={index}
+          className={`questionBtn ${index === active ? "active" : ""}`}
+          onClick={() => handleButtonClick(index)}
+        >
+            {" "}
+            {answer}{" "}
           </button>
+          // <Button key={index} variant="contained">
+          //   {answer}
+          // </Button>
         );
       })}
     </div>
   );
 };
 
-export default Question;
+export default Question;
